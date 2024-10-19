d4cc80e46c160f6e896f6fd42be7a82b0335716e
Girish G Gonda <girishggonda@Girishs-MacBook-Pro.local>
2024-10-19 17:12:04 +0530

    added registration

--- src/main/java/com/engati/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/engati/ecommerce/config/SecurityConfig.java
@@ -2,19 +2,35 @@
 //
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+//import org.springframework.security.config.http.SessionCreationPolicy;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.DefaultSecurityFilterChain;
 //import org.springframework.security.web.SecurityFilterChain;
 //
 //@Configuration
 //@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfiguration {
+//@EnableMethodSecurity
+//public class SecurityConfig {
 //
 //
 //    @Bean
 //    public BCryptPasswordEncoder passwordEncoder() {
 //        return new BCryptPasswordEncoder();
 //    }
+////    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+////        return httpSecurity
+////                .csrf(csrf -> csrf.disable())
+////                .authorizeHttpRequests(auth -> auth
+////                        .requestMatchers("/api/**").permitAll()
+////                        .anyRequest().authenticated()
+////                )
+////                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+////                .httpBasic(Customizer.withDefaults())
+////                .build();
+////    }
 //}
